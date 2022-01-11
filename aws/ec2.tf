2ececcbc7432ff16c63a4cdcaad4e59f6d1942b8
Michal Nyga <mnyga94@gmail.com>
2022-01-11 11:48:13 +0000

    Adding comments and output to get previously used private IP

--- aws/ec2.tf
+++ aws/ec2.tf
@@ -1,9 +1,16 @@
 provider "aws" {
     region = "us-east-1"
+    #Local profile for provider.
     profile = "panda"
 }
 
+# Free tier
 resource "aws_instance" "panda"{
     ami = "ami-04505e74c0741db8d"
     instance_type = "t2.micro"
+}
+
+# Get private_ip from previous run.
+output "my_private_ip" {
+    value = aws_instance.panda.private_ip
 }
